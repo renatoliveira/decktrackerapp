79335f44e68a073de85b51fcd0f49786a9cb6e5e
Renato Oliveira <renato.oliveira@dogeforce.com>
2024-11-27 00:14:01 -0300

    adds deck modification record and tests

--- djangoapp/decktracker/models.py
+++ djangoapp/decktracker/models.py
@@ -14,6 +14,37 @@ class Deck(models.Model):
     def __str__(self):
         return self.name
 
+    def add_card(self, card, quantity):
+        if CardDeck.objects.filter(card=card, deck=self).exists():
+            cd = CardDeck.objects.get(card=card, deck=self)
+            cd.quantity += quantity
+            cd.save()
+        else:
+            CardDeck.objects.create(card=card, deck=self, quantity=quantity)
+
+        DeckModification.objects.create(deck=self, card=card, quantity=-quantity)
+
+    def add_cards(self, cards):
+
+        for card, quantity in cards.items():
+            self.add_card(card, quantity)
+
+    def remove_card(self, card, quantity):
+        cd = CardDeck.objects.get(card=card, deck=self)
+
+        if cd.quantity <= quantity:
+            cd.delete()
+        else:
+            cd.quantity -= quantity
+            cd.save()
+
+        DeckModification.objects.create(deck=self, card=card, quantity=-quantity)
+
+    def remove_cards(self, cards):
+
+        for card, quantity in cards.items():
+            self.remove_card(card, quantity)
+
 class CardDeck(models.Model):
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
@@ -25,14 +56,8 @@ class CardDeck(models.Model):
 class DeckModification(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'Modification of deck {self.deck.name} on {self.date}'
-
-class DeckModificationCard(models.Model):
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
-    deck_modification = models.ForeignKey(DeckModification, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
     def __str__(self):
-        return f'{self.card.name} in {self.deck_modification.deck.name} on {self.deck_modification.date}'
+        return f'Modification of deck {self.deck.name} on {self.date}'
--- djangoapp/decktracker/tests.py
+++ djangoapp/decktracker/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from decktracker.models import *
 
-# Create your tests here.
+class DeckSetupTestCase(TestCase):
+    def setUp(self):
+        Deck.objects.create(name='Test Deck')
+        Card.objects.create(sf_id='1', name='Test Card')
+
+    def test_add_card_to_deck(self):
+        deck = Deck.objects.get(name='Test Deck')
+        card = Card.objects.get(name='Test Card')
+        deck.add_card(card, 1)
+
+        self.assertEqual(CardDeck.objects.get(card=card, deck=deck).quantity, 1)
+        self.assertEqual(DeckModification.objects.filter(deck=deck, card=card).count(), 1)
+
+    def test_remove_card_from_deck(self):
+        deck = Deck.objects.get(name='Test Deck')
+        card = Card.objects.get(name='Test Card')
+        deck.add_card(card, 1)
+        deck.remove_card(card, 1)
+
+        self.assertFalse(CardDeck.objects.filter(card=card, deck=deck).exists())
+        self.assertEqual(DeckModification.objects.filter(deck=deck, card=card).count(), 2)
+
+    def test_add_multiple_cards_to_deck(self):
+        deck = Deck.objects.get(name='Test Deck')
+        card = Card.objects.get(name='Test Card')
+        deck.add_card(card, 1)
+        deck.add_card(card, 2)
+
+        self.assertEqual(CardDeck.objects.get(card=card, deck=deck).quantity, 3)
+        self.assertEqual(DeckModification.objects.filter(deck=deck, card=card).count(), 2)
+
+    def test_remove_multiple_cards_from_deck(self):
+        deck = Deck.objects.get(name='Test Deck')
+        card = Card.objects.get(name='Test Card')
+        deck.add_card(card, 3)
+        deck.remove_card(card, 2)
+
+        self.assertEqual(CardDeck.objects.get(card=card, deck=deck).quantity, 1)
+        self.assertEqual(DeckModification.objects.filter(deck=deck, card=card).count(), 2)
