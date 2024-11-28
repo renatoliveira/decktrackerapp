843f6979785202a3fa4b76a2df0f6bcd3fed8a90
Renato Oliveira <renato.oliveira@dogeforce.com>
2024-11-28 07:28:17 -0300

    adds custom css to static files

--- djangoapp/decktracker/urls.py
+++ djangoapp/decktracker/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:deck_id>/', views.view_deck, name='view_deck'),
+    path('deck/<int:deck_id>/', views.view_deck, name='view_deck'),
 ]
