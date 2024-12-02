a49d799a079a999fb489a2e0dcb5cff7bc081766
Renato Oliveira <renato.oliveira@dogeforce.com>
2024-12-02 20:23:29 -0300

    adds registration page

--- djangoapp/decktracker/urls.py
+++ djangoapp/decktracker/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('deck/<int:deck_id>/', views.view_deck, name='view_deck'),
+    path('register/', views.register_view, name='register'),
+    path('user/register', views.register_view, name='register'),
+    path('user/login', views.login_view, name='login'),
 ]
--- djangoapp/decktracker/views.py
+++ djangoapp/decktracker/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Deck
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
@@ -20,3 +22,45 @@ def view_deck(request, deck_id):
     }
 
     return render(request, "decks/deck.html", context)
+
+def register_view(request):
+    if request.method == 'POST':
+
+        # make sure attributes are not empty
+        if not request.POST['username'] or not request.POST['email'] or not request.POST['password']:
+            return render(request, "registration/registration_form.html", {'error': 'All fields are required'})
+
+        try:
+            user = User.objects.create_user(
+                request.POST['username'],
+                request.POST['email'],
+                request.POST['password']
+            )
+
+            user.save()
+        except Exception as e:
+            return render(request, "registration/registration_form.html", {'error': f'Error creating user'})
+
+        return render(request, "registration/registration_form.html", {'success': 'Registration Successful'})
+
+    else:
+        return render(request, "registration/register.html")
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = User.objects.filter(username=username)
+
+        if user.count() == 0:
+            render(request, "registration/login.html", {'error': 'Invalid username or password'})
+
+        user = user[0]
+
+        if user.check_password(password):
+            render(request, "registration/login.html", {'error': 'Invalid username or password'})
+        else:
+            render(request, "registration/login.html", {'error': 'Invalid username or password'})
+
+    return render(request, "registration/login.html")
