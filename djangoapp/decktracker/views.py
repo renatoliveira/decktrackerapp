6a9540b7af34baff0831f9d51d2570293f565bb3
Renato Oliveira <renato.oliveira@dogeforce.com>
2024-12-04 00:49:56 -0300

    sort imports and improve delete deck label

--- djangoapp/decktracker/views.py
+++ djangoapp/decktracker/views.py
@@ -1,8 +1,8 @@
+from .models import Deck
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .models import Deck
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
